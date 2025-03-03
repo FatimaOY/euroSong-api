37c6ab8c411f36a9a4eb5a99cf4606a65a157952
FatimaOY <r1000778@student.thomasmore.be>
2025-03-03 11:22:09 +0100

    finished songs

--- routes/artists.js
+++ routes/artists.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const {PrismaClient } = require('@prisma/client'); 
+const prisma = new PrismaClient();
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const data = [];
